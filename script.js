f1766c982bf3d212f2028caea952106a3140c64e
bondad-ui <laurencebondad2@gmail.com>
2025-01-16 13:07:44 +0800

    header 100%

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const burgir = document.querySelector(".toggle__menu")
+const burgir = document.querySelector(".toggle__menu");
+const headernav = document.querySelector(".header .header__nav");
 burgir.addEventListener("click", () => {
 burgir.classList.toggle("open");    
+headernav.classList.toggle("open");    
 });
